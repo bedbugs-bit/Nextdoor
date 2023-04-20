3f690287aa2e671f52578f5f37d4d2902cd95f0b
Joel Obowu <57295185+UZzoma@users.noreply.github.com>
2023-04-20 14:53:12 +0300

    create mongoose schema and auth

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import { register } from "./controllers/auth.js";
 
 
 
@@ -18,7 +19,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginEmbedderPolicy({policy: "cross-origin"}));
+app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}));
 app.use(morgan("common"));
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
@@ -36,3 +37,20 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({storage});
+
+
+// ROUTES WITH FILES
+app.post("/auth/register", upload.single("picture", register));
+
+
+// MONGOOSE DB SETUP
+const PORT = process.env.PORT || 6001;
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true}).then(() => {
+        app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+    }).catch((error) => console.log(`${error} did not connect`));
+
+
+
+
